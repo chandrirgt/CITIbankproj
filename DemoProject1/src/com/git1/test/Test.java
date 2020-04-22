261bdd077b4d2a34a23a9db9d5bf3d286eb8bf56
dev2 <Dev2@gmail.com>
2020-04-22 15:18:17 +0530

    dev1 modified test.java

--- DemoProject1/src/com/git1/test/Test.java
+++ DemoProject1/src/com/git1/test/Test.java
@@ -1,5 +1,12 @@
 package com.git1.test;
 
+import com.git1.beans.Utility;
+
 public class Test {
 
+	public static void main(String[] args) {
+		Utility ut=new Utility();
+		System.out.println(ut.giveDate());
+		
+	}
 }
